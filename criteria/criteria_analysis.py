cedbc06460a07354e5953cf32db198dca3b1e8ea
hsuBnOediH <li.8906@buckeyemail.osu.edu>
2024-12-16 08:18:50 -0800

    Update criteria_analysis.py
    C3

--- criteria/criteria_analysis.py
+++ criteria/criteria_analysis.py
@@ -26,17 +26,13 @@ for subject in subjects:
     if len(temp_list) == 1:
         subject_files[subject] = temp_list[0]
     elif len(temp_list) > 1:
-        print('--'*20)
-        print('Multiple files for subject:', subject)
         # sort the files by date
         temp_list.sort()
-        print('Files:', temp_list)
         for current_file in temp_list:
             # check if the file is complete by last row of file is start with 359 or not
             df = pd.read_csv('/mnt/dell_storage/labs/NPC/DataSink/StimTool_Online/WB_Advice/'+current_file)
             if df.iloc[-1, 0] == 359:
                 subject_files[subject] = current_file
-                print(f"Selecting file {current_file} for subject {subject}")
                 break
             else:
                 print(f"File {current_file} for subject {subject} is incomplete")
@@ -52,12 +48,13 @@ subject_data = {}
 for subject, file in subject_files.items():
     df = pd.read_csv('/mnt/dell_storage/labs/NPC/DataSink/StimTool_Online/WB_Advice/'+file)
     subject_data[subject] = df
-print(f"Read {len(subject_data)} dataframes")
+
 
 # for each dataframe, find the last row whose trial_type is MAIN, and remove rows before that, including that row
 for subject, df in subject_data.items():
     last_main_row = df[df['trial_type'] == 'MAIN'].index[-1]
     subject_data[subject] = df.iloc[last_main_row+1:]
+print(f"Read {len(subject_data)} dataframes")
 
 
 
@@ -70,12 +67,14 @@ for subject, df in subject_data.items():
     incorrect = 0
     for index, row in df.iterrows():
         if row['event_type'] == 9:
+            if row['result'] in ('try left', 'try right'):
+                continue
             if int(row['result']) > 0:
                 correct += 1
             else:
                 incorrect += 1
     criteria1[subject] = {'correct': correct, 'incorrect': incorrect}
-print(criteria1)
+print(f"finished criteria1")
 
 
 
@@ -93,5 +92,22 @@ for subject, df in subject_data.items():
             elif row['response'] == 'right':
                 right += 1
     criteria2[subject] = {'left': left, 'right': right}
-print(criteria2)
+print(f"finished criteria2")
 
+
+# check criteria 3
+# reaction time defined as the time between event_type 8 and 5
+# for each subject, calculate the average reaction time
+criteria3 = {}
+for subject, df in subject_data.items():
+    reaction_times = []
+    for index, row in df.iterrows():
+        if row['event_type'] == 8:
+            start_time = row['absolute_time']
+        elif row['event_type'] == 5:
+            end_time = row['absolute_time']
+            reaction_times.append(end_time - start_time)
+    criteria3[subject] = {'average_reaction_time': sum(reaction_times)/len(reaction_times), 'reaction_times': reaction_times}
+print(f"finished criteria3")
+for subject, data in criteria3.items():
+    print(subject, data['average_reaction_time'])
