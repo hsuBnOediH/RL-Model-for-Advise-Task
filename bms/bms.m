594da4b14c5d514a2fcc970076fc8192398e87e9
hsuBnOediH <li.8906@buckeyemail.osu.edu>
2025-06-12 07:29:17 +0800

    set up framework to get RL LL for BCI eval

--- bms/bms.m
+++ bms/bms.m
@@ -37,7 +37,7 @@ end
 addpath(spmPath);
 
 % input_file = 'L:/rsmith/lab-members/ttakahashi/WellbeingTasks/AdviceTask/output.csv';
-input_file = '../outputs/combined_with_without_log_active_inference_F_table.csv';
+input_file = '../inputs/active_inference_policysF_table_filtered.csv';
 % output_file = 'L:/rsmith/lab-members/ttakahashi/WellbeingTasks/AdviceTask/';
 output_folder_path = '';
 % addpath('L:/rsmith/all-studies/util/spm12/');
